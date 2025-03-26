88845aa8ce4d090756180644d9606d8cddb245e3
Rarcand <rarcand3969@gmail.com>
2025-03-26 17:55:04 -0500

    Started Login page

--- PT-App/app/_layout.tsx
+++ PT-App/app/_layout.tsx
@@ -28,10 +28,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
+      <Stack initialRouteName="login"> 
+        {/* expo-router automatically finds login.tsx */}
+        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
